487edc89b6cfaf79f4f9798e893dacc18e396e87
Amy Stapleton <amylaura2002@ymail.com>
2024-10-10 13:08:28 +0100

    testing function and fixing it's any errors

--- bad.R
+++ bad.R
@@ -2,10 +2,13 @@ fun = function(one)
 {
   two <- one + 1
   four <- matrix(1:10,nrow = 2)
-  four[ 1, ]
+  print(four[ one, ])
   txt <- 'hi'
   three <- two + 1
   if(txt == 'hi'){
     three
   }
 }
+
+fun(2)
+fun(1)
